35e993ca099ad816ea3056461a6080474b16b2a8
Matthew Dunbar <matthewdunbar11@gmail.com>
2019-06-08 23:46:08 -0400

    Configure calendar and event admin

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,28 @@ class Event < ApplicationRecord
   validates :title, :start, :end, presence: true
 
   has_rich_text :description
+  
+  rails_admin do
+    configure :created_at do
+      visible false
+    end
+
+    configure :description do
+      visible false
+    end
+
+    configure :start do
+      strftime_format '%B %d, %Y %I:%M %p'
+      datepicker_options do 
+        { showTodayButton: false, format: parser.to_momentjs }
+      end
+    end
+
+    configure :end do
+      strftime_format '%B %d, %Y %I:%M %p'
+      datepicker_options do 
+        { showTodayButton: false, format: parser.to_momentjs }
+      end
+    end
+  end
 end
