628a0577b9b2c73a043c7371af0ce44940085b8a
Matthew Dunbar <matthewdunbar11@gmail.com>
2019-06-11 19:58:22 -0400

    Finish styling and admin

--- app/controllers/home_controller.rb
+++
@@ -1,3 +0,0 @@
-class HomeController < ApplicationController
-  layout 'narrow'
-end
---
+++ web/app/models/event.rb
@@ -0,0 +1,39 @@
+class Event < ApplicationRecord
+  belongs_to :calendar
+
+  validates :title, :start, :end, presence: true
+
+  has_rich_text :description
+  
+  rails_admin do
+    configure :id do
+      visible false
+    end
+
+    configure :created_at do
+      visible false
+    end
+
+    configure :updated_at do
+      visible false
+    end
+
+    configure :description do
+      visible false
+    end
+
+    configure :start do
+      strftime_format '%B %d, %Y %I:%M %p'
+      datepicker_options do 
+        { showTodayButton: false, format: parser.to_momentjs }
+      end
+    end
+
+    configure :end do
+      strftime_format '%B %d, %Y %I:%M %p'
+      datepicker_options do 
+        { showTodayButton: false, format: parser.to_momentjs }
+      end
+    end
+  end
+end
